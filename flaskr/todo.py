c778a6889de5d6875bbff82c8bf87797a7eafe59
Allan Assis <allan.andrade@corp.globo.com>
2019-08-13 15:42:05 -0300

    feat: add other routes to todo.py

--- flaskr/todo.py
+++ flaskr/todo.py
@@ -1,8 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, request
+import json
 
 bp = Blueprint("todo", __name__, url_prefix="/todo")
 
 
 @bp.route("/")
-def todo():
+def todo_list()i:
+    '''
+        Get list
+    '''
     return "todo-list"
+
+@bp.route("/", methods=["POST"])
+def todo_post():
+    '''
+        Add a new item
+    '''
+    data = request.json
+    return data
+
+@bp.route("/<key>", methods=["PUT"])
+def todo_put(key):
+    '''
+        Update an item
+    '''
+    data = request.json
+    data["key"] = key
+    return data
+
+@bp.route("/<key>", methods=["DELETE"])
+def todo_delete(key):
+    '''
+        Update an item
+    '''
+
+    data = request.json
+    data["key"] = key
+    return data
