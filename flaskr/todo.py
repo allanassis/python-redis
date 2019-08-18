5d085d165832dee3ff62d0ca4f760f29c909e7f7
Allan Assis <allan.andrade@corp.globo.com>
2019-08-17 21:14:29 -0300

    fix: incorrect caracter

--- flaskr/todo.py
+++ flaskr/todo.py
@@ -1,38 +1,40 @@
 from flask import Blueprint, request
-import json
 
 bp = Blueprint("todo", __name__, url_prefix="/todo")
 
 
 @bp.route("/")
-def todo_list()i:
-    '''
+def todo_list():
+    """
         Get list
-    '''
+    """
     return "todo-list"
+
 
 @bp.route("/", methods=["POST"])
 def todo_post():
-    '''
+    """
         Add a new item
-    '''
+    """
     data = request.json
     return data
 
+
 @bp.route("/<key>", methods=["PUT"])
 def todo_put(key):
-    '''
+    """
         Update an item
-    '''
+    """
     data = request.json
     data["key"] = key
     return data
 
+
 @bp.route("/<key>", methods=["DELETE"])
 def todo_delete(key):
-    '''
+    """
         Update an item
-    '''
+    """
 
     data = request.json
     data["key"] = key
