5c95510aafa003c4245e02e1c47d846b09cfa70f
Allan Assis <allan.andrade@corp.globo.com>
2019-08-17 22:48:50 -0300

    refactor: add config file

---
+++ flaskr/config.py
@@ -0,0 +1,10 @@
+REDIS_URL = "redis://localhost:6379"
+
+schema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "last_name": {"type": "string"},
+        "age": {"type": "number"},
+    },
+}
